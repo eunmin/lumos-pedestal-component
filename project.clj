d809f10fe675ed7244e4d31c1aa0bbbb78add4dd
Eunmin Kim <telepopsound@gmail.com>
2015-08-12 11:59:48 +0900

    Remove logger for production

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lumos-pedestal-component "0.1.0-SNAPSHOT"
+(defproject lumos-pedestal-component "0.1.1-SNAPSHOT"
   :description "A component for pedestal"
   :url "https://github.com/eunmin/lumos-pedestal-component"
   :license {:name "The MIT License"
@@ -8,10 +8,9 @@
                  [io.pedestal/pedestal.service "0.4.0"]
                  [io.pedestal/pedestal.jetty "0.4.0"]
                  [io.pedestal/pedestal.immutant "0.4.0"]
-                 [io.pedestal/pedestal.tomcat "0.4.0"]
-                 [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.7"]
-                 [org.slf4j/jcl-over-slf4j "1.7.7"]
-                 [org.slf4j/log4j-over-slf4j "1.7.7"]]
-  :resource-paths ["config"]
-  :profiles {:dev {:dependencies [[clj-http "2.0.0"]]}})
+                 [io.pedestal/pedestal.tomcat "0.4.0"]]
+  :profiles {:dev {:dependencies [[clj-http "2.0.0"]
+                                  [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
+                                  [org.slf4j/jul-to-slf4j "1.7.7"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.7"]
+                                  [org.slf4j/log4j-over-slf4j "1.7.7"]]}})
